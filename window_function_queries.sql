def1d4daa3bcea2ea7189a8c1e6e0bb5d09e2412
Rob Taylor <63524882+dataforyounz@users.noreply.github.com>
2023-09-25 10:57:07 +1300

    Rewrite to accommodate new table format

--- window_function_queries.sql
+++ window_function_queries.sql
@@ -2,7 +2,7 @@
 select
 	"year"
 	,host_country
-	,winner
+	,first_place
 	,total_goals
 	,row_number() over() as row_num
 from world_cup_placings;
@@ -11,7 +11,7 @@ from world_cup_placings;
 select
 	"year"
 	,host_country
-	,winner
+	,first_place
 	,total_goals
 	,sum(total_goals) over() as all_goals
 from world_cup_placings;
@@ -20,7 +20,7 @@ from world_cup_placings;
 select
 	"year"
 	,host_country
-	,winner
+	,first_place
 	,total_goals
 	,round(avg(total_goals) over(), 0) as mean_goals
 from world_cup_placings;
@@ -29,7 +29,7 @@ from world_cup_placings;
 select
 	"year"
 	,host_country
-	,winner
+	,first_place
 	,total_goals
 	,round(avg(total_goals) over(), 0) as mean_goals
 	,round((total_goals - avg(total_goals) over()) / stddev(total_goals) over(), 2) as z_score
@@ -39,42 +39,42 @@ from world_cup_placings;
 select
 	"year"
 	,host_country
-	,winner
+	,first_place
 	,total_goals
-	,row_number() over( partition by winner ) as row_num
+	,row_number() over( partition by first_place ) as row_num
 from world_cup_placings;
 
 /* Adding more window functions to the SELECT list */
 select
 	"year"
 	,host_country
-	,winner
-	,row_number() over( partition by winner ) as row_num
+	,first_place
+	,row_number() over( partition by first_place ) as row_num
 	,total_goals
-	,sum(total_goals) over( partition by winner ) as all_goals
+	,sum(total_goals) over( partition by first_place ) as all_goals
 	,total_attendance
-	,max(total_attendance) over( partition by winner ) as max_attednance
+	,max(total_attendance) over( partition by first_place ) as max_attendance
 from world_cup_placings;
 
 /* Same as above, but now using the WINDOW clause */
 select
 	"year"
 	,host_country
-	,winner
+	,first_place
 	,row_number() over w as row_num
 	,total_goals
 	,sum(total_goals) over w as all_goals
 	,total_attendance
-	,max(total_attendance) over w as max_attednance
+	,max(total_attendance) over w as max_attendance
 from world_cup_placings
-window w as ( partition by winner )
+window w as ( partition by first_place )
 ;
 
 /* Comparing ROW_NUMBER and RANK using ORDER BY clause */
 select
 	"year"
 	,host_country
-	,winner
+	,first_place
 	,total_goals
 	,row_number() over( order by total_goals desc ) as row_num
 	,rank() over( order by total_goals desc ) as row_rank
@@ -84,21 +84,21 @@ from world_cup_placings;
 select
 	"year"
 	,host_country
-	,winner
-	,rank() over( partition by winner order by "year" desc) as row_rank
+	,first_place
+	,rank() over( partition by first_place order by "year" desc) as row_rank
 from world_cup_placings
 ;
 
 /* Filter only rows that have a rank of 1 */
 select
 	"year"
-	,winner
+	,first_place
 from (
 	select
 		"year"
 		,host_country
-		,winner
-		,rank() over( partition by winner order by "year" desc) as row_rank
+		,first_place
+		,rank() over( partition by first_place order by "year" desc) as row_rank
 	from world_cup_placings
 ) as subq
 where row_rank = 1
